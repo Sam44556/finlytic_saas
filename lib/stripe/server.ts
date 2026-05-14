14105b8a86faad6a2ecb8016aa434d108226c1b4
sami-g <samuelgirma888@gmail.com>
2026-05-14 15:50:52 +0300

    Update Stripe API version to 2026-04-22.dahlia

--- lib/stripe/server.ts
+++ lib/stripe/server.ts
@@ -5,6 +5,6 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2024-12-18.acacia',
+  apiVersion: '2026-04-22.dahlia',
   typescript: true,
 });
